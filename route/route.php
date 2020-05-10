efd83d37806f15ec7adc5e1ff4a9916fbe31c63b
CrowdYellow <echo61546@gmail.com>
2020-05-10 17:54:40 +0800

    v1.0.5 后台登录页与首页路由

--- route/route.php
+++ route/route.php
@@ -2,4 +2,12 @@
 
 use \think\facade\Route;
 
-Route::get('admins', 'admin/Admins/index');
+# 后台登陆页面
+Route::group('backstage', function () {
+    # 后台登录页面
+    Route::get('/login', 'admin/Login/index');
+    # 后台登录页面
+    Route::get('/', 'admin/Index/index');
+    # 管理员
+    Route::get('admins', 'admin/Admins/index');
+});
